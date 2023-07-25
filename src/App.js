95df5f29d664658a4cf446e574f0df03c05685eb
mustafairshaidd <mustafa-irshaid_2001@hotmail.com>
2023-07-25 11:40:49 +0300

    Firing Up The Project == first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
