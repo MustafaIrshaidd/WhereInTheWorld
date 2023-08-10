1d407ed93dd7a9d9cd8b5b7d4fb413a314b6d315
mustafa-irshaid-harri <mustafa.irshaid.113@gmail.com>
2023-08-10 12:27:55 +0300

    hot fixes

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -23,9 +23,7 @@ const Navbar = () => {
 
       <header className="shadow-sm">
         <div className="centralized-container m-auto d-flex justify-content-between align-items-center py-4">
-          <Link to="/WhereInTheWorld">
-            <h1 className="m-0 text-white">Where in the world?</h1>
-          </Link>
+          <h1 className="m-0">Where in the world?</h1>
         </div>
       </header>
     </>
